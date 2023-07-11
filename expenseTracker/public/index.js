4f31d348d4216f5cd5fe22385bf6d404b3a87911
Vaggtzm <111343674+Vaggtzm@users.noreply.github.com>
2023-07-11 21:41:04 +0300

    expense tracker update
    charts added , total expense calculation and multiple budgets

--- expenseTracker/public/index.js
+++ expenseTracker/public/index.js
@@ -10,42 +10,83 @@
     //print the expense
     let budgetDisplay = document.getElementById('amount');
     budgetDisplay.innerText = Newbudget;
+  // Call the function to update the chart
+  updateChart(budgetDisplay);
  });
+ //display the budget to chart 
+ function updateChart(budgetDisplay) {
+  const budgetCanvas = document.getElementById('BudgetChart');
+  const chart = new Chart(budgetCanvas, {
+    type: 'pie',
+    data: {
+      labels: ['Current Budget'],
+      datasets: [{
+        label: 'Expense Chart',
+        data: [parseFloat(budgetDisplay.innerText)],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.5)', // Current budget bar color
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
 
+// Set initial total expense to 0
+let totalExpense = 0;
 
- //set new expense value and calculate the remaining budget 
+// Get the expense button
+const expenseButton = document.getElementById('check-amount');
 
- //get the expense  button 
- const ExpenseButton = document.getElementById('check-amount');
- // event listener for the expense button
- ExpenseButton.addEventListener('click', () => {
-   // get the expense input
-   const productCost = document.getElementById('expense_value');
-   // get the expenditure display
-   let expenseDisplay = document.getElementById('expense_display');
-   // read the value of the expense input
-   const newExpense = productCost.value;
-   // then update the expenditure display
-   expenseDisplay.innerText = newExpense;
- 
-   // calculate the remaining budget and total expense
-   // get the remaining budget display
-   const RemainingBudget = document.getElementById('Remaining_budget');
-   // get the budget display
-   const TotalBudget = document.getElementById('amount');
-   // perform the subtraction
-   if (RemainingBudget.innerText > 0) {
-     RemainingBudget.innerText = parseFloat(RemainingBudget.innerText) - parseFloat(productCost.value);
-   } else {
-     RemainingBudget.innerText = TotalBudget.innerText - newExpense;
-   }
- 
-   // calculate the total expense
-   const expenseFull = document.getElementById('total_expense');
-   expenseFull.innerText = parseFloat(expenseDisplay.innerText) + parseFloat(productCost.value);
- 
-   // console checks
-   console.log(productCost.value);
-   console.log(RemainingBudget.innerText);
-   console.log(parseFloat(RemainingBudget.innerText) - parseFloat(productCost.value));
- });
+// Event listener for the expense button
+expenseButton.addEventListener('click', () => {
+  // Get the expense input
+  const productCost = document.getElementById('expense_value');
+
+  // Get the expenditure display
+  const expenseDisplay = document.getElementById('expense_display');
+
+  // Read the value of the expense input
+  const newExpense = parseFloat(productCost.value);
+
+  // Update the expenditure display
+  expenseDisplay.innerText = newExpense;
+
+  // Calculate the remaining budget and total expense
+  // Get the remaining budget display
+  const remainingBudget = document.getElementById('Remaining_budget');
+
+  // Get the budget display
+  const totalBudget = document.getElementById('amount');
+
+  // Perform the subtraction for remaining budget
+  if (parseFloat(remainingBudget.innerText) > 0) {
+    remainingBudget.innerText = parseFloat(remainingBudget.innerText) - newExpense;
+  } else {
+    remainingBudget.innerText = parseFloat(totalBudget.innerText) - newExpense;
+  }
+
+  // Add the new expense to the total expense
+  totalExpense += newExpense;
+
+  // Update the total expense display
+  const expenseFull = document.getElementById('total_expense');
+  expenseFull.innerText = totalExpense;
+
+       // Console checks
+  console.log(productCost.value);
+  console.log(remainingBudget.innerText);
+  console.log(parseFloat(remainingBudget.innerText) - newExpense);
+
+  });
+
